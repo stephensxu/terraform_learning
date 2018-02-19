4b4893852f6d6887abc2f90c7efcb73cc9c1acbf
stephensxu <stephensxub@gmail.com>
2018-02-18 17:32:06 -0800

    add private subnet route table

--- vpc.tf
+++ vpc.tf
@@ -91,3 +91,21 @@ resource "aws_subnet" "us-west-2a-private" {
     Name = "Private Subnet"
   }
 }
+
+resource "aws_route_table" "us-west-2a-private" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    instance_id = "${aws_instance.nat.id}"
+  }
+
+  tags {
+    Name = "Private Subnet"
+  }
+}
+
+resource "aws_route_table_association" "us-west-2a-private" {
+  subnet_id = "${aws_subnet.us-west-2a-private.id}"
+  route_table_id = "${aws_route_table.us-west-2a-private.id}"
+}
