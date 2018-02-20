dbca5cdfe9b5cefa548a8baadb7bf072ee151486
stephensxu <stephensxub@gmail.com>
2018-02-19 22:50:13 -0800

    added egress config to SG

--- private.tf
+++ private.tf
@@ -1,14 +1,39 @@
-resource "aws_instance" "private_subnet_instance_1" {
+resource "aws_security_group" "db" {
+  name = "vpc_db"
+  description = "Allow incoming database connections."
+
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = "${aws_vpc.default.id}"
+
+  tags {
+    Name = "DBServerSG"
+  }
+}
+
+resource "aws_instance" "db-1" {
   ami = "${lookup(var.amis, var.aws_region)}"
   availability_zone = "us-west-2a"
   instance_type = "t2.micro"
   key_name = "${var.aws_key_name}"
-  vpc_security_group_ids = ["${aws_security_group.nat.id}"]
+  vpc_security_group_ids = ["${aws_security_group.db.id}"]
   subnet_id = "${aws_subnet.us-west-2a-public.id}"
   associate_public_ip_address = false
   source_dest_check = false
 
   tags {
-    Name = "Private subnet Server 1"
+    Name = "DB Server 1"
   }
 }
